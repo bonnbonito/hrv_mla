8d46ef10c356f96c970f2f6e3d0fe629f39500c2
bonn <bonnbonito@gmail.com>
2023-03-29 22:57:06 +0800

    fix faq tabs

--- README.txt
+++ README.txt
@@ -65,6 +65,9 @@ directory take precedence. For example, `/assets/screenshot-1.png` would win ove
 
 == Changelog ==
 
+= 2.2.3 =
+* Fix FAQ Tabs
+
 = 2.2.2 =
 * Add Date picker to other date fields
 
--- public/js/hrv_mla-public.js
+++ public/js/hrv_mla-public.js
@@ -1,32 +1,17 @@
-(function( $ ) {
-	'use strict';
+window.addEventListener("DOMContentLoaded", (event) => {
+	const questionWrapper = document.querySelectorAll('.question-wrapper');
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
-
-})( jQuery );
+	questionWrapper.forEach( question => {
+		question.addEventListener('click', e => {
+			e.preventDefault();
+			if ( question.classList.contains('active') ) return;
+			let q = question.getAttribute('id');
+			let parent = question.closest('.faq-wrapper');
+			
+			parent.querySelectorAll('.question-wrapper').forEach( question => question.classList.remove('active') );
+			parent.querySelectorAll('.answer-wrapper').forEach( answer => answer.classList.remove('active') );
+			question.classList.add('active');
+			document.querySelector('#' + q + 'c').classList.add('active');
+		});
+	})
+});
