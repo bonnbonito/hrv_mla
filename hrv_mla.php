661bb5359d38a67c0a32014a45b63be526cd627c
bonn <bonnbonito@gmail.com>
2023-07-11 21:13:15 +0800

    version

--- hrv_mla.php
+++ hrv_mla.php
@@ -16,7 +16,7 @@
  * Plugin Name:       HRV - MLA Plugin
  * Plugin URI:        https://www.mlawebdesigns.co.uk
  * Description:       Plugin for HRV Website
- * Version:           2.2.26
+ * Version:           2.2.300
  * Author:            Bonn Joel Elimanco
  * Author URI:        https://www.linkedin.com/in/bonn-joel-elimanco-56a43a20
  * License:           GPL-2.0+
